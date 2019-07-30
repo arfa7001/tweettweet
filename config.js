94a511b70f5f362ba21db9f0391aaaba8cd9346a
Faris <faris.armaly@nielsen.com>
2019-07-30 17:53:46 -0400

    adding all

---
+++ config.js
@@ -0,0 +1,12 @@
+// Change this file to config.js
+// Add your keys
+// Add file .gitignore: config.js
+// Load with 
+// var config = require('./config.js');
+
+module.exports = {
+    consumer_key:         'TLtPeoZISUXNp6zc3zu6uenZ6',
+    consumer_secret:      'rC6KPLtETGqfBLsZvCfv3evclnIyPgTvPtHy2N39o9OrBaqmxq',
+    access_token:         '1156305861293293571-P5lzb3V7UV9s3HN7Nz5sGAn13GzVKc',
+    access_token_secret:  '9txdk9xnsEaf7vJzaKlT8cCJ8YWMiEMotixf2sfYwG5vq'
+}
--- index.js
+++ index.js
@@ -1,17 +1,32 @@
-var Twit = require('twit')
-var T = new Twit({
-    consumer_key:         'TLtPeoZISUXNp6zc3zu6uenZ6',
-    consumer_secret:      'rC6KPLtETGqfBLsZvCfv3evclnIyPgTvPtHy2N39o9OrBaqmxq',
-    access_token:         '1156305861293293571-P5lzb3V7UV9s3HN7Nz5sGAn13GzVKc',
-    access_token_secret:  '9txdk9xnsEaf7vJzaKlT8cCJ8YWMiEMotixf2sfYwG5vq'
-})
-var users = ["1005179256589176833"];
-var stream = T.stream('statuses/filter', {follow: users});
-stream.on('tweet', function (tweet) {
-    if (users.indexOf(tweet.user.id_str) > -1) {
-        console.log(tweet.user.name + ": " + tweet.text);
-        T.post('statuses/retweet/:id', { id: tweet.id_str }, function (err, data, response) {
-            console.log(data)
-        })
+// Using the Twit node package
+// https://github.com/ttezel/twit
+var Twit = require('twit');
+
+// Pulling all my twitter account info from another file
+var config = require('./config.js');
+
+var T = new Twit(config);
+
+tweeter();
+setInterval(tweeter, 60*5*1000);
+
+// Here is the bot!
+function tweeter() {
+
+  // This is a random number bot
+  var tweet = 'Here\'s a random number between 0 and 100: ' + Math.floor(Math.random()*100);
+
+  // Post that tweet!
+  T.post('statuses/update', { status: tweet }, tweeted);
+
+  // Callback for when the tweet is sent
+  function tweeted(err, data, response) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Success: ' + data.text);
+      //console.log(response);
     }
-})
+  };
+
+}
